a51bdb4eb91f70c271c319b34c5548b8282af1c6
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2021-12-28 16:34:29 +0200

    ternary operator value assignment for tipChanged method

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,6 +9,9 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = sender == zeroPctButton ? true : false
+        tenPctButton.isSelected = sender == tenPctButton ? true : false
+        twentyPctButton.isSelected = sender == twentyPctButton ? true : false
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
