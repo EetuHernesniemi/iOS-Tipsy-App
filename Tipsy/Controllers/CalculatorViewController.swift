7c642e2fe8f9d810bff087328be3ee39e6ab3678
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2021-12-30 09:56:27 +0200

    passing values bu seque

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -1,13 +1,16 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
-
+    
+    var tipAmount : Float = 0
+    var splitValue : Float = 2.0
+    var percentageDescription : String = "0%"
+    
     @IBAction func tipChanged(_ sender: UIButton) {
         billTextField.endEditing(true)
         zeroPctButton.isSelected = sender == zeroPctButton ? true : false
@@ -25,16 +28,28 @@ class CalculatorViewController: UIViewController {
         billTextField.endEditing(true)
         if billTextField.text != nil {
             let billValue : Float = Float(billTextField.text?.description ?? "0.0") ?? 0.0
-            let splitValue : Float = Float(splitNumberLabel.text?.description ?? "2") ?? 2
+            splitValue = Float(splitNumberLabel.text?.description ?? "2") ?? 2
             if zeroPctButton.isSelected {
-                print((billValue*0)/splitValue)
+                tipAmount = (billValue*0)/splitValue
+                percentageDescription = "0%"
             }
             if tenPctButton.isSelected {
-                print((billValue*0.1)/splitValue)
+                tipAmount = (billValue*0.1)/splitValue
+                percentageDescription = "10%"
             }
             if twentyPctButton.isSelected {
-                print((billValue*0.2)/splitValue)
+                tipAmount = (billValue*0.2)/splitValue
+                percentageDescription = "20%"
             }
+            self.performSegue(withIdentifier: "goToResult", sender: self)
+        }
+    }
+    
+    override func prepare(for seque: UIStoryboardSegue, sender: Any?) {
+        if seque.identifier == "goToResult" {
+            let destinationVC = seque.destination as! ResultsViewController
+            destinationVC.totalPerPerson = String(format: "%.2f", tipAmount)
+            destinationVC.settingsDescription = "Split between \(Int(splitValue).description) people with \(percentageDescription) tip."
         }
     }
 }
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -1,30 +1,21 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    var totalPerPerson : String?
+    var settingsDescription : String?
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        totalLabel.text = totalPerPerson
+        settingsLabel.text = settingsDescription
     }
     
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
